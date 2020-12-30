957d5b1e15c01fbeb95112a7d27ad07d78cea782
michael-iskander-java <icl236@outlook.com>
2020-12-30 10:28:54 +0200

    Accepted IQ Test

--- AmazonCodingInterviewQuestion/src/com/michael/iskander/problemsolving/amazoninterview/IQTest.java
+++ AmazonCodingInterviewQuestion/src/com/michael/iskander/problemsolving/amazoninterview/IQTest.java
@@ -1,28 +1,43 @@
 package com.michael.iskander.problemsolving.amazoninterview;
-
+import java.util.Scanner;
 //https://codeforces.com/contest/328/problem/A
 public class IQTest {
 
 	public static void main(String[] args) {
 
+
+		Scanner z=new Scanner(System.in);
+		String line = z.nextLine();
+
+		String [] arrayOfNumberString = line.split(" ");
+
+
+
+
 		int[] a = new int[4];
 
 
-		a[0] = 1;
-		a[1] = 334;
-		a[2] = 667;
-		a[3] = 1000;
+		a[0] = Integer.valueOf(arrayOfNumberString[0]);
+		a[1] = Integer.valueOf(arrayOfNumberString[1]);
+		a[2] = Integer.valueOf(arrayOfNumberString[2]);
+		a[3] = Integer.valueOf(arrayOfNumberString[3]);
+
+
 
 		int d = a[1] - a[0];
 
+
+
 		double r = (double)a[1]/(double)a[0];
 
+		int i = (int) (a[3]*r);
+
 		if (a[2]-a[1] == d && a[3]-a[2] == d)
-			System.out.println("The result is: " + (a[3]+d));
-		else if ((double)a[2]/(double)a[1] == r && (double)a[3]/(double)a[2] == r && (double)a[3]*r==a[3]*r)
-			System.out.println("The result is: " + (a[3]*r));
+			System.out.println(a[3]+d);
+		else if ((double)a[2]/(double)a[1] == r && (double)a[3]/(double)a[2] == r && ((int) (a[3]*r)== (a[3]*r)))
+			System.out.println((int)(a[3]*r));
 		else
-			System.out.println("The result is: " + 42);
+			System.out.println(42);
 	}
 
 }
